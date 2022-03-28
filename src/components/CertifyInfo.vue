48a9fed1bbaa0b89023c4d59e5f8beeaa654c1bc
Leon Kist <kist@mpdl.mpg.de>
2022-03-28 14:02:16 +0200

    Form validation

--- src/components/CertifyInfo.vue
+++ src/components/CertifyInfo.vue
@@ -11,13 +11,13 @@
             <v-text-field v-model="authorName" v-on:change="nameChange" label="Author or Group Name" hint="Enter your group or author(s) research was conducted with" persistent-hint outline></v-text-field>
           </v-flex>
           <v-flex xs12 md12 md6 lg6 class="my-3 text-xs-center" align-center>
-            <v-text-field v-model="mintAddress" v-on:change="mintaddressChange" label="Bloxberg Address" hint="Enter your bloxberg address that you would like the certification to be minted to." persistent-hint outline></v-text-field>
+            <v-text-field v-model="mintAddress" v-on:change="mintaddressChange" :rules="mintRules" :success="!!mintAddress" label="Bloxberg Address" hint="Enter your bloxberg address that you would like the certification to be minted to (Format: 0x...)." persistent-hint outline></v-text-field>
           </v-flex>
           <v-flex xs12 md12 md6 lg6 class="my-3 text-xs-center" align-center>
             <v-text-field v-model="researchTitle" v-on:change="titleChange" label="Title or Brief Description of Research" hint="Enter a brief description of what the data entails" persistent-hint outline></v-text-field>
           </v-flex>
           <v-flex xs12 md12 md6 lg6 class="my-3 text-xs-center" align-center>
-            <v-text-field v-model="emailAddress" v-on:change="emailChange" label="Email Address" hint="If you wish an email address to be associated with the data" persistent-hint outline></v-text-field>
+            <v-text-field v-model="emailAddress" v-on:change="emailChange" :rules="emailRules" :success="!!emailAddress" label="Email Address" hint="If you wish an email address to be associated with the data" persistent-hint outline></v-text-field>
           </v-flex>
         </v-layout>
       </v-container>
@@ -36,6 +36,12 @@
           researchTitle: '',
           mintAddress: '',
           emailAddress: '',
+          emailRules: [
+            v => (v.match(/(?:.+@.+\..+)?/) && v.match(/(?:.+@.+\..+)?/)[0] === v) || 'Invalid E-Mail (will not be used)'
+          ],
+          mintRules: [
+            v => (v.match(/(?:0x.{40})?/) && v.match(/(?:0x.{40})?/)[0] === v) || 'Invalid Address, required format: 0x... (will not be used)'
+          ],
           valid: ''
         }
       },
@@ -55,13 +61,21 @@
           this.$store.state.user.authorName = this.authorName
         },
         mintaddressChange: function () {
-          this.$store.state.user.mintAddress = this.mintAddress
+          if (v.match(/(?:0x.{40})?/) && v.match(/(?:0x.{40})?/)[0] === v) {
+            this.$store.state.user.mintAddress = this.mintAddress
+          } else {
+            this.$store.state.user.mintAddress = ''
+          }
         },
         titleChange: function () {
           this.$store.state.user.researchTitle = this.researchTitle
         },
         emailChange: function () {
-          this.$store.state.user.emailAddress = this.emailAddress
+          if (v.match(/(?:.+@.+\..+)?/) && v.match(/(?:.+@.+\..+)?/)[0] === v) {
+            this.$store.state.user.emailAddress = this.emailAddress
+          } else {
+            this.$store.state.user.emailAddress = ''
+          }
         }
       }
     }
